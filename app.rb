e00d0ceaf71fb88c15ee2f8ee4406eb051d5b481
kshakirov <kshakirov@zoral.com.ua>
2016-01-04 13:18:51 +0200

    initial commmit

---
+++ app.rb
@@ -0,0 +1,13 @@
+puts "hi"
+require 'active_record'
+require 'sqlite3'
+require 'logger'
+
+ActiveRecord::Base.logger = Logger.new('debug.log')
+configuration = YAML::load(IO.read('config/database.yml'))
+ActiveRecord::Base.establish_connection(configuration['development'])
+
+class User < ActiveRecord::Base
+end
+
+p User.all
