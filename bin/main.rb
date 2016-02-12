913becb1821549eac14072d4bfeec0dfaa5ff992
kshakirov <kshakirov@zoral.com.ua>
2016-02-12 12:49:22 +0200

    WIP #1 bin var created

---
+++ bin/main.rb
@@ -0,0 +1,4 @@
+require_relative "../lib/app_helper"
+require_relative "../lib/app_lib"
+
+p User.all
--- main.rb
+++
@@ -1,4 +0,0 @@
-require_relative "lib/app_helper"
-require_relative "lib/app_lib"
-
-p User.all
