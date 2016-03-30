968ebce6495a775e9fa0be296451b685f864d942
Peter <pbsandler@gmail.com>
2016-03-30 11:43:14 +0000

    almost ready

--- test/test_dummy.rb
+++ test/test_dummy.rb
@@ -1,10 +1,14 @@
 
-require_relative "test_helper"
-
-
-class TestDummy < Minitest::Unit::TestCase
-  def test_dummy
-    actual = Chair.all
-    assert_instance_of  Chair::ActiveRecord_Relation, actual
+class A
+  def test_method
+    puts "it is test method"
+    yield "This is a yield"
   end
 end
+
+
+
+a = A.new
+a.test_method {|i| puts  "The yield says: #{i}"}
+
+
